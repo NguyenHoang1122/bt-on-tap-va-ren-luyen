84f2444db4fdfcbfbd6e003777631685e72964e6
NguyenHoang1122 <nguyenhoang9xbn@gmail.com>
2025-05-05 00:30:10 +0700

    bai11

---
+++ js/bai11.js
@@ -0,0 +1,128 @@
+let listProduct = [];
+function displayProducts() {
+    let tbody = document.getElementById('container-product');
+    tbody.innerHTML = '';
+    for (let i = 0; i < listProduct.length; i++) {
+        let student = listProduct[i];
+        let row = '<tr>';
+        row += '<td>' + student.ma + '</td>';
+        row += '<td>' + student.ten + '</td>';
+        row += '<td>' + student.lop + '</td>';
+        row += '<td>' + student.email + '</td>';
+        row += '<td>' + student.ngaysinh + '</td>';
+        row += '</tr>';
+        tbody.innerHTML += row;
+    }
+}
+function checkIdStudent(ma){
+    let regex = /^HV-\d{4}$/;
+    return regex.test(ma);
+}
+function checkBirthday(ngaysinh){
+    let regex = /^\d{2}\/\d{2}\/\d{4}$/;
+    return regex.test(ngaysinh);
+}
+function addStudent(){
+    let idStudent = prompt('Nhập mã sinh viên theo định dạng HV-XXXX :');
+    if(!checkIdStudent(idStudent)){
+        alert('Mã học viên không đúng hãy nhập đúng định dạng HV-XXXX.');
+        return;
+    }
+    for(let i = 0; i < listProduct.length; i++){
+        if(listProduct[i].ma == idStudent){
+            alert('Mã học viên này đã tồn tại.');
+            return;
+        }
+    }
+    let nameStudent = prompt('Hãy nhập tên học viên của bạn(tối đa 50 ký tự) :');
+    if(nameStudent.length > 50){
+        alert('Tên của bạn quá dài.');
+        return;
+    }
+    let classStudent = prompt('Nhập lớp của bạn :');
+    let emailStudent = prompt('Nhập địa chỉ Email của bạn :');
+    let birthdayStudent = prompt('Nhập ngày sinh của bạn theo đúng định dạng dd/mm/yyyy :');
+    if(!checkBirthday(birthdayStudent)){
+        alert('Bạn đã nhập sai định dang.');
+        return;
+    }
+    let student = {
+        ma : idStudent,
+        ten : nameStudent,
+        lop : classStudent,
+        email : emailStudent,
+        ngaysinh : birthdayStudent,
+    };
+    listProduct.push(student);
+    displayProducts();
+}
+function deleteStudent(){
+    let idStudent = prompt('Nhập mã học viên cần xóa (HV-XXXX) :');
+    let findStudent = false;
+    if(!checkIdStudent(idStudent)){
+        alert('Bạn đã nhập sai mã học viên.');
+        return;
+    }
+    for(let i = 0; i < listProduct.length; i++){
+        if(listProduct[i].ma == idStudent){
+            let xacnhan = confirm('Bạn có chắc chắn muốn xóa ?');
+            if(xacnhan){
+                listProduct.splice(i, 1);
+                displayProducts();
+            }
+            findStudent = true;
+            break;
+        }
+        if(!findStudent){
+            alert('Học viên này không tồn tại.');
+        }
+    }
+}
+
+function updateStudent(){
+    let idStudent = prompt('Nhập mã học viên cần sửa (HV-XXXX) :');
+    let findStudent = false;
+    if(!checkIdStudent(idStudent)){
+        alert('Bạn đã nhập sai mã học viên.');
+        return;
+    }
+    for(let i = 0; i < listProduct.length; i++){
+        if(listProduct[i].ma == idStudent){
+            let nameStudent = prompt('Hãy nhập tên học viên của bạn(tối đa 50 ký tự) :',listProduct[i].ten);
+            if(nameStudent.length > 50){
+                alert('Tên của bạn quá dài.');
+                return;
+            }
+            let classStudent = prompt('Nhập lớp của bạn :',listProduct[i].lop);
+            let emailStudent = prompt('Nhập địa chỉ Email của bạn :',listProduct[i].email);
+            let birthdayStudent = prompt('Nhập ngày sinh của bạn theo đúng định dạng dd/mm/yyyy :',listProduct[i].ngaysinh);
+            if(!checkBirthday(birthdayStudent)){
+                alert('Bạn đã nhập sai định dang.');
+                return;
+            }
+            listProduct[i].ten = nameStudent;
+            listProduct[i].lop = classStudent;
+            listProduct[i].email = emailStudent;
+            listProduct[i].ngaysinh = birthdayStudent;
+            displayProducts();
+            findStudent = true;
+            break;
+        }
+    }
+    if(!findStudent){
+        alert('Mã học viên này không tồn tại.');
+    }
+}
+let snowflakes = 100;
+
+for (let i = 0; i < snowflakes; i++) {
+    let snowflake = document.createElement("div");
+    snowflake.className = "snowflake";
+    snowflake.textContent = "❄";
+    snowflake.style.left = Math.random() * window.innerWidth + "px";
+    snowflake.style.fontSize = Math.random() * 10 + 10 + "px";
+    snowflake.style.animationDuration = Math.random() * 5 + 5 + "s";
+    snowflake.style.opacity = Math.random();
+    snowflake.style.animationDelay = Math.random() * 10 + "s";
+    document.body.appendChild(snowflake);
+}
